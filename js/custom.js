93754d259fce276e0ae0f049a35194f9d135a856
Alfredo Barron <alfredobarronc@gmail.com>
2016-07-12 17:21:42 -0500

    Update v2.0.0
    - Change design

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,19 @@
 // --------------------------------------------------------
 // Pretty Photo for Lightbox Image
-// -------------------------------------------------------- 
-$(document).ready(function() {	
-    $("a[data-gal^='prettyPhoto']").prettyPhoto(); 
+// --------------------------------------------------------
+$(document).ready(function() {
+    $("a[data-gal^='prettyPhoto']").prettyPhoto();
+    new WOW().init();
 });
 
 // --------------------------------------------------------
 //	Navigation Bar
-// -------------------------------------------------------- 	
-$(window).scroll(function(){	
-	"use strict";	
+// --------------------------------------------------------
+$(window).scroll(function(){
+	"use strict";
 	var scroll = $(window).scrollTop();
-	if( scroll > 60 ){		
-		$(".navbar").addClass("scroll-fixed-navbar");				
+	if( scroll > 60 ){
+		$(".navbar").addClass("scroll-fixed-navbar");
 	} else {
 		$(".navbar").removeClass("scroll-fixed-navbar");
 	}
@@ -20,10 +21,19 @@ $(window).scroll(function(){
 
 // --------------------------------------------------------
 //	Smooth Scrolling
-// -------------------------------------------------------- 	
+// --------------------------------------------------------
 $(".navbar-nav li a[href^='#']").on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $(this.hash).offset().top
     }, 1000);
+});
+
+$('#carousel').on('slid.bs.carousel', function (event) {
+    var nextactiveslide = $(event.relatedTarget).index();
+    var $btns = $('.steps-list');
+    var $active = $btns.find("[data-slide-to='" + nextactiveslide + "']");
+    $btns.find('li').removeClass('active');
+    $active.addClass('active');
+
 });
